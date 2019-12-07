d69ecf39ced26f081963bce358c54ce5b5fd3f43
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2019-12-07 11:45:44 -0300

    Finalizado app Frases do Dia

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,78 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
 
-  runApp(app);
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var _title = "Frases do dia";
+  var _frases = [
+    "Frase 1",
+    "Frase 2",
+    "Frase 3",
+    "Frase 4"
+  ];
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase() {
+    var numeroSorteado = Random().nextInt( _frases.length );
+    print(numeroSorteado);
+
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/logo.png"),
+              Text(
+                _fraseGerada,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    fontSize: 17,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black
+                ),
+              ),
+              RaisedButton(
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                color: Colors.green,
+                onPressed: _gerarFrase,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
